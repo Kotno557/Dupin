49c77e36848216554777d85f1271417aecdf4eae
Dyang <testzzo.no1@gmail.com>
2023-09-17 23:43:30 +0800

    edit CLI

--- dupin_client.py
+++ dupin_client.py
@@ -1,8 +1,18 @@
 from lib.dupin_python_lib.dupin_core import DupinPathSniffer, DupinInfoSniffer, DupinLevelGrader, DupinVchainConnecter, database_init
+import sys
 
 if __name__ == '__main__':
-    a: DupinVchainConnecter = DupinVchainConnecter('kemono.party')
-    a.connect()
+    database_init()
+    main()
 
-    # a = DupinPathSniffer('google.com')
-    # print()
+def main():
+    try:
+        if sys.argv[1] == 'path':
+            target: str = sys.argv[2]
+            temp: DupinPathSniffer = DupinPathSniffer(targit_url = target)
+            print(temp.sniff_result)
+            temp2: DupinInfoSniffer = DupinInfoSniffer(temp)
+            print(temp2.info_result)
+        
+    except:
+        print("command error")
